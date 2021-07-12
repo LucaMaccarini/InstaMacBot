6c0f6dd560f03c1875d8beafd42a834ca5fce7a6
MaccariniLuca <31884823+MaccariniLuca@users.noreply.github.com>
2021-07-12 21:42:32 +0200

    2.0 beta testing

--- InstaMacBot/InstaMacBot/Fill_Forms/new_bot.Designer.cs
+++ InstaMacBot/InstaMacBot/Fill_Forms/new_bot.Designer.cs
@@ -49,8 +49,9 @@ namespace InstaMacBot.DesktopInterface
             this.pn_main.Controls.Add(this.lb_1);
             this.pn_main.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pn_main.Location = new System.Drawing.Point(0, 0);
+            this.pn_main.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pn_main.Name = "pn_main";
-            this.pn_main.Size = new System.Drawing.Size(695, 548);
+            this.pn_main.Size = new System.Drawing.Size(927, 674);
             this.pn_main.TabIndex = 2;
             this.pn_main.Resize += new System.EventHandler(this.pn_main_Resize);
             // 
@@ -60,11 +61,12 @@ namespace InstaMacBot.DesktopInterface
             this.lb_4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.lb_4.Dock = System.Windows.Forms.DockStyle.Top;
             this.lb_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_4.Location = new System.Drawing.Point(0, 407);
-            this.lb_4.MaximumSize = new System.Drawing.Size(695, 0);
+            this.lb_4.Location = new System.Drawing.Point(0, 461);
+            this.lb_4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_4.MaximumSize = new System.Drawing.Size(927, 0);
             this.lb_4.Name = "lb_4";
-            this.lb_4.Padding = new System.Windows.Forms.Padding(2);
-            this.lb_4.Size = new System.Drawing.Size(694, 100);
+            this.lb_4.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lb_4.Size = new System.Drawing.Size(922, 120);
             this.lb_4.TabIndex = 16;
             this.lb_4.Text = resources.GetString("lb_4.Text");
             // 
@@ -74,11 +76,12 @@ namespace InstaMacBot.DesktopInterface
             this.lb_3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.lb_3.Dock = System.Windows.Forms.DockStyle.Top;
             this.lb_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_3.Location = new System.Drawing.Point(0, 307);
-            this.lb_3.MaximumSize = new System.Drawing.Size(695, 0);
+            this.lb_3.Location = new System.Drawing.Point(0, 341);
+            this.lb_3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_3.MaximumSize = new System.Drawing.Size(927, 0);
             this.lb_3.Name = "lb_3";
-            this.lb_3.Padding = new System.Windows.Forms.Padding(2);
-            this.lb_3.Size = new System.Drawing.Size(458, 100);
+            this.lb_3.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lb_3.Size = new System.Drawing.Size(572, 120);
             this.lb_3.TabIndex = 15;
             this.lb_3.Text = "\r\nmy contacts to give me suggestions of new bots\r\nemail: lucamaccarini22@gmail.co" +
     "m\r\ntelegram: @Luca_macca";
@@ -89,11 +92,12 @@ namespace InstaMacBot.DesktopInterface
             this.lb_2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.lb_2.Dock = System.Windows.Forms.DockStyle.Top;
             this.lb_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_2.Location = new System.Drawing.Point(0, 135);
-            this.lb_2.MaximumSize = new System.Drawing.Size(695, 0);
+            this.lb_2.Location = new System.Drawing.Point(0, 134);
+            this.lb_2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_2.MaximumSize = new System.Drawing.Size(927, 0);
             this.lb_2.Name = "lb_2";
-            this.lb_2.Padding = new System.Windows.Forms.Padding(2);
-            this.lb_2.Size = new System.Drawing.Size(673, 172);
+            this.lb_2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lb_2.Size = new System.Drawing.Size(927, 207);
             this.lb_2.TabIndex = 14;
             this.lb_2.Text = resources.GetString("lb_2.Text");
             // 
@@ -105,9 +109,10 @@ namespace InstaMacBot.DesktopInterface
             this.bt_manage_lists.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bt_manage_lists.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bt_manage_lists.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(65)))), ((int)(((byte)(103)))));
-            this.bt_manage_lists.Location = new System.Drawing.Point(0, 100);
+            this.bt_manage_lists.Location = new System.Drawing.Point(0, 91);
+            this.bt_manage_lists.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.bt_manage_lists.Name = "bt_manage_lists";
-            this.bt_manage_lists.Size = new System.Drawing.Size(695, 35);
+            this.bt_manage_lists.Size = new System.Drawing.Size(927, 43);
             this.bt_manage_lists.TabIndex = 13;
             this.bt_manage_lists.Text = "Click here and check GitHub!";
             this.bt_manage_lists.UseVisualStyleBackColor = false;
@@ -120,10 +125,11 @@ namespace InstaMacBot.DesktopInterface
             this.lb_1.Dock = System.Windows.Forms.DockStyle.Top;
             this.lb_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lb_1.Location = new System.Drawing.Point(0, 0);
-            this.lb_1.MaximumSize = new System.Drawing.Size(695, 0);
+            this.lb_1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_1.MaximumSize = new System.Drawing.Size(927, 0);
             this.lb_1.Name = "lb_1";
-            this.lb_1.Padding = new System.Windows.Forms.Padding(2);
-            this.lb_1.Size = new System.Drawing.Size(691, 100);
+            this.lb_1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lb_1.Size = new System.Drawing.Size(910, 91);
             this.lb_1.TabIndex = 4;
             this.lb_1.Text = "Desktop client bot version: 2.0 (beta testing) see if there is a new release on G" +
     "itHub!\r\nIf you like the bot and want to support me leave me a star the repositir" +
@@ -131,11 +137,12 @@ namespace InstaMacBot.DesktopInterface
             // 
             // new_bot
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(65)))), ((int)(((byte)(103)))));
-            this.ClientSize = new System.Drawing.Size(695, 548);
+            this.ClientSize = new System.Drawing.Size(927, 674);
             this.Controls.Add(this.pn_main);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "new_bot";
             this.Text = "New Bot";
             this.Load += new System.EventHandler(this.new_bot_Load);
